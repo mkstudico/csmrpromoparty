cfa519e43804806384becc6fc125f199942c1e7d
mkstudico <mkstudio841@gmail.com>
2026-04-07 20:15:20 +0200

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+{
+  "type": "service_account",
+  "project_id": "akademia-c56ff",
+  "private_key_id": "2b73319dcb24968a2f951eb9937e8aa072a1a257",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQD4Aa7gdlbJRBZB\nR9e80LSNeFRn/tMvjeltqHjiOEzF+GrBPw9NLOh5fRcp3coNZPyB6QLbn/wsSLkR\n6oAq3+82ixGtUKUPPPFd+Q+U2osHAjs64T8+8qmEzV+okrwWvwyEUGVXFjDqAprU\nryOvq4JxZtlpNal7SWqyxY0qoXHlKLblnJzAI6b9WD5Py3gS9vD3Y7WMiSPe/HhY\n2MH1jB/+zBbcOjcjBwSoigkOS6Ud/XvkHar74b2C3NdGc6ayvHnw3KKOGbfyL+X8\n1r14AqDZZQuXVUEcwsCgjri+M7X1lbZnFEDtzw4OkofcwzJvOB1CNiCfiObIaU7K\nGZ/FzOZ5AgMBAAECggEADhcuY+NlV+cN6qSVzcz/zR/ej9ALGKMT9Z6CS7wOJJtS\nGWunR2e7R5fDVqYawjsQG6Ns/aRv0+ZH2Yw8vvLdKPiqxvGThzObtQ6fA4KNLpdC\nXTyJ9t4bood2nWHE3Pf60pOsxk9k9Q6z1EylFuiEL0Q8Kv2ZBP+tV73goODuUyop\n5D5Tzb9VjeJIqpB5/c00jFWK/ew+sJMks55kdWAJZ80DOy7Vq4yT/8XIAfv3WvQ/\nbIQoTY3/NoSDlORJ0nblwfVsCRT167SuwLxt8v14yWL7zIbjZUfSE/wlKKIv3U3r\n99UbEYAcDORtRSto0emRotLPlrR1BkjjoIVCraJs9wKBgQD8AoKSX8bmx+1hqDzL\nSCpZ8Ig3x9Gkw0LxEkFR5YQ5fCbfXPBbaxVF7svLod9RmLQQuOyl8GqlVD9mqe33\n2jtFjmGEGUff8RQVwCDiYdNNmUYPwYoL5nYq01cOoVMQRy9QyqZpAK4iir0zC6Iw\nTvzt1j01anpdc6l/ePsWD4r9fwKBgQD77vJMX6pROiNbGdBu07k2Sg6Egi60pQAn\nEq1LtWGIz9NhxD4lZRSZZziICT/aultNe93x2Sbx65meB2gsZ6HhlQk7F2tvzGcZ\nq6FJO9URiGVOBy355sWiOY5Z/zivV3FYHjqPl2HI1EZ8njSRsYNNpVmXQmRBS9E+\noibgsOwIBwKBgBRWe4Uml52+Wsj709QwAcL6Z3Pg+2lJnNhpub3LQQ2GmHiEPp2S\nGchoCihn6ubh5P5IfkiyC1lGYEt/wUW0Zf4qDHKlu6g2A4Hx3HWIoY3spbvVybdG\nJRk1rxvuJw70P7xMbnug42zEN5qsAK3XYhArrwrlifSKxg4UjGbuYDAvAoGBAO95\neqhOZz8+3EzEhPRUHUIVsycR+AOOtE5v2nqUt1ms82mDeO9i3enTzFDDOgdYDWHh\n1F8H45UiNquYrtVGzQH5AQVChcWHrpkgI/fQ///ifylkebeKPWKvAzlY+TDzFbKF\nSXUkmLal0Tqzx5SWABHWkwrM+S94OjYnPH1or3QRAoGAe58+42vhoPxF1JClLYhl\ndd7agW03V2BVbqIDQXtz+k59bA33Pifvu5L7yr0GfClrgTefc3Z9z+MfLdZOFnC2\nAIyadXCMpgkQ63jbM7sJuC2KLhy/NjYl1IaCgTLXg2sG+vsmUUXE5k3gRPPf9FP9\nJVTIAVBNnFvb8cT9uP4aor8=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@akademia-c56ff.iam.gserviceaccount.com",
+  "client_id": "113814879770580490211",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40akademia-c56ff.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
